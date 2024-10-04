92349c5ff0841c162a3978eb81221ca430a49497
nazimfeni <mdnazimuddin77@gmail.com>
2024-10-04 20:10:36 +0600

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
 // src/components/Header.js
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <h1 className="text-2xl md:text-3xl font-bold mb-4 md:mb-0">Md. Nazim Uddin</h1>
+        <h1 className="text-2xl md:text-3xl font-bold mb-4 md:mb-0">
+          Md. Nazim Uddin
+        </h1>
         <nav className="flex space-x-4">
-          <a href="#about" className="hover:text-blue-500">About</a>
-          <a href="#projects" className="hover:text-blue-500">Projects</a>
-          <a href="#contact" className="hover:text-blue-500">Contact</a>
+          <a href="#about" className="hover:text-blue-500">
+            About
+          </a>
+          <a href="#projects" className="hover:text-blue-500">
+            Projects
+          </a>
+          <a href="#contact" className="hover:text-blue-500">
+            Contact
+          </a>
+          <a href="#gallery" className="hover:text-blue-500">
+            Gallery
+          </a>
         </nav>
       </div>
     </header>
