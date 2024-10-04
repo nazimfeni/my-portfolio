5bd5fe725fd611bca6cc7b789d4fdfa95f3297a4
nazimfeni <mdnazimuddin77@gmail.com>
2024-10-04 21:27:14 +0600

    image uploaded

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,6 +3,10 @@ import React from "react";
 
 const images = [
   {
+    src: `${process.env.PUBLIC_URL}/images/Training_session.jpg`,
+    alt: "Ina Training Session",
+  },
+  {
     src: `${process.env.PUBLIC_URL}/images/Graduationnight.jpg`,
     alt: "IBA JU Graduation Night",
   },
@@ -15,16 +19,20 @@ const images = [
     alt: "JU Campus",
   },
   {
-    src: `${process.env.PUBLIC_URL}/images/Training_session.jpg`,
-    alt: "Ina Training Session",
-  },
-  {
     src: `${process.env.PUBLIC_URL}/images/Sara.jpg`,
     alt: "My Daughter Sara",
   },
   {
     src: `${process.env.PUBLIC_URL}/images/Gaming_time.jpg`,
-    alt: "GAming Time",
+    alt: "Gaming Time",
+  },
+  {
+    src: `${process.env.PUBLIC_URL}/images/Inauguration_RMS.jpg`,
+    alt: "Inauguration RMS",
+  },
+  {
+    src: `${process.env.PUBLIC_URL}/images/MomoInn_Swimming.jpg`,
+    alt: "Momo Inn Swimming Time",
   },
 ];
 
@@ -33,13 +41,16 @@ const Gallery = () => {
     <section id="gallery" className="py-16 bg-white">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-8">Gallery</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {images.map((image, index) => (
-            <div key={index} className="overflow-hidden rounded-lg shadow-md">
+            <div
+              key={index}
+              className="flex items-center justify-center overflow-hidden rounded-lg shadow-md h-80"
+            >
               <img
                 src={image.src}
                 alt={image.alt}
-                className="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105"
+                className="w-80 h-80 object-cover transition-transform duration-300 transform hover:scale-105"
               />
             </div>
           ))}
